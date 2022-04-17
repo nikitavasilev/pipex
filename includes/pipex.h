8ffa81993355e10b3fa4d1e5794393627cb73934
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-04-17 04:53:20 +0200

    ++extract_path()

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:53:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/15 23:53:54 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/04/17 04:50:06 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+char	**get_paths(char *const envp[]);
+
+#endif
---
+++ srcs/extract_path.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extract_path.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 04:46:58 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/04/17 04:51:07 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "libft.h"
+
+static char	*get_path_envp(char *const *env)
+{
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = ft_strdup(env[i] + 5);
+			return (path);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+
+char	**get_paths(char *const envp[])
+{
+	char	*path;
+	char	**paths;
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	path = get_path_envp(envp);
+	if (!path)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	paths = ft_split(path, ':');
+	while (paths[i])
+	{
+		tmp = paths[i];
+		paths[i] = ft_strjoin(tmp, "/");
+		free(tmp);
+		i++;
+	}
+	return (paths);
+}
