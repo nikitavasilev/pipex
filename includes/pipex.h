ac7a8c2cbf0b523493affeedf3f00203e0ed8b77
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-04-17 10:44:55 +0200

    test struct

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,12 +6,20 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:53:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/17 06:18:47 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/04/17 08:35:33 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+typedef struct s_args
+{
+	char	**paths;
+	char	***cmds;
+	int		infile;
+	int		outfile;
+}	t_args;
 
 char	**get_paths(char *const envp[]);
 char	***get_cmds(int argc, char *const argv[]);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:54:03 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/17 04:51:24 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/04/17 08:38:14 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,64 @@
 		parent_process(outfile, argv[4]);
 }*/
 
+void	pipex(int infile, int outfile, char *const argv[], char *const envp[])
+{
+	int		end[2];
+	pid_t	parent;
+
+	pipe(end);
+	parent = fork();
+	if (parent < 0)
+		return (perror("Fork:"));
+	if (!parent)
+		child_process(infile, argv[1]);
+	else
+		parent_process(outfile, argv[argc - 1]);
+}
+
 int	main(int argc, char *const argv[], char *const envp[])
 {
 	char	**paths;
+	char	***cmds;
+	int		infile;
+	int		outfile;
+	t_args	args;
 
-	(void)argc;
-	(void)argv;
-	(void)envp;
-	paths = get_paths(envp);
-	while (paths[0])
+	args = (t_args) {
+		.paths = get_paths(envp),
+		.cmds = get_cmds(argc, argv),
+		.infile = 0,
+		.outfile = 1
+	};
+
+	if (argc >= 5)
 	{
-		printf("%s\n", paths[0]);
-		paths++;
+		infile = open(argv[1], O_RDONLY);
+		outfile = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (infile < 0 || outfile < 0)
+			return (perror("Open:"), 1);
+		paths = get_paths(envp);
+		if (!paths)
+			return (perror("get_paths:"), 1);
+		cmds = get_cmds(argc, argv);
+		if (!cmds)
+			return (perror("get_cmds:"), 1);
+		pipex(infile, outfile, envp, paths, cmds);
+		/*while (paths[0])
+		{
+			printf("%s\n", paths[0]);
+			paths++;
+		}
+		i = 0;
+		while (cmds[i])
+		{
+			while (cmds[i][0])
+			{
+				printf("%s\n", cmds[i][0]);
+				cmds[i]++;
+			}
+			i++;
+		}*/
 	}
 	return (0);
 }
