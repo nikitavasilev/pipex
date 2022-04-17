2cc90d5fc536935332886dfa48bccd9429ede9c2
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-04-17 06:51:17 +0200

    extract cmds from args

---
+++ srcs/get_cmds.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_cmds.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 06:17:06 by nvasilev          #+#    #+#             */
+/*   Updated: 2022/04/17 06:17:44 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	***get_cmds(int argc, char *const argv[])
+{
+	int		i;
+	char	***cmds;
+
+	cmds = (char ***)malloc(sizeof(char **) * (argc - 2));
+	cmds[2] = NULL;
+	i = 0;
+	while (i < argc - 3)
+	{
+		cmds[i] = ft_split(argv[i + 2], ' ');
+		if (!cmds[i])
+		{
+			while (i--)
+				free(cmds[i]);
+			free(cmds);
+			return (NULL);
+		}
+		i++;
+	}
+	return (cmds);
+}
