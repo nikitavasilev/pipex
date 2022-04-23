6bb450684a6553efaf1382b2fa9eef5fa1fdd92e
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-04-23 06:09:08 +0200

    ++cmd_count

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:53:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/17 08:35:33 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/04/23 04:37:06 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 typedef struct s_args
 {
-	char	**paths;
-	char	***cmds;
-	int		infile;
-	int		outfile;
+	char			**paths;
+	char			***cmds;
+	int				infile;
+	int				outfile;
+	unsigned int	cmd_count;
 }	t_args;
 
 char	**get_paths(char *const envp[]);
