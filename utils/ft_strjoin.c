8a396e733beba8a85777f84becd0f641add017e8
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-11 23:48:04 +0200

    norm v3

--- utils/ft_strjoin.c
+++ utils/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 14:39:31 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/04/17 04:25:38 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/09 18:12:34 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 
 	if (!s1 || !s2)
-		return (0);
-	if (!(str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
-		return (0);
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
