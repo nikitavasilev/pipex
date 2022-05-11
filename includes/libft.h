182670a9f747ab4a548021ab98c0ce23a254624d
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-11 23:39:15 +0200

    rm substr + edit putstr

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:53:56 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/02 21:17:19 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/10 10:33:34 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 # include <stddef.h>
 
 size_t	ft_strlen(const char *s);
-int	ft_putchar_fd(int c, int fd);
-void	ft_putstr_fd(const char *str, int fd);
+int		ft_putchar_fd(int c, int fd);
+int		ft_putstr_fd(const char *str, int fd);
 char	**ft_split(char const *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
