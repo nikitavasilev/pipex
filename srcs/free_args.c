7175e6af49a964687f77e135690ba45c778103f2
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-14 03:34:23 +0200

    ++free_err_join() to handle join crash

--- srcs/free_args.c
+++ srcs/free_args.c
@@ -6,14 +6,25 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:14:57 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/09 17:17:38 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/14 02:21:25 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "pipex.h"
 
-static void	free_tab(char **tab)
+void	free_err_join(char *path, char *tmp, char **paths)
+{
+	size_t	i;
+
+	free(tmp);
+	free(path);
+	i = 0;
+	while (paths[i])
+		free(paths[i++]);
+}
+
+void	free_tab(char **tab)
 {
 	unsigned int	i;
 
@@ -47,4 +58,5 @@ void	free_args(t_args args)
 {
 	free_cmds(args.cmds);
 	free_tab(args.paths);
+	free(args.cpid);
 }
