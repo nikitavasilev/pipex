cf0d161ede09e905f8a1b4bf88eb62b86a010987
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-14 03:36:42 +0200

    null handling

--- srcs/is_dir.c
+++ srcs/is_dir.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:37:18 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/09 17:46:24 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/12 02:20:16 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ int	is_dir(char *path)
 {
 	int	fd;
 
+	if (!path)
+	{
+		errno = EINVAL;
+		return (errno);
+	}
 	fd = open(path, O_RDWR);
 	if ((fd == -1) && (errno == EISDIR))
 	{
-		errno = EACCES;
-		return (EISDIR);
+		errno = EISDIR;
+		return (errno);
 	}
 	if (fd > 0)
 		close(fd);
