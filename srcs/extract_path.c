82637846eafeecd91c50b6ce49b14e588036a76f
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-14 03:33:15 +0200

    norm -> split_path sep

--- srcs/extract_path.c
+++ srcs/extract_path.c
@@ -6,15 +6,14 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 04:46:58 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/01 09:21:50 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/14 02:29:03 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
 #include "libft.h"
+#include "pipex.h"
 
 static char	*get_path_envp(char *const *env)
 {
@@ -34,6 +33,25 @@ static char	*get_path_envp(char *const *env)
 	return (NULL);
 }
 
+static char	**split_path(char *path)
+{
+	char	**paths;
+
+	if (!path)
+	{
+		print_err(errno, __FILE__, __LINE__ - 3);
+		return (NULL);
+	}
+	paths = ft_split(path, ':');
+	if (!paths)
+	{
+		free(path);
+		print_err(errno, __FILE__, __LINE__ - 3);
+		return (NULL);
+	}
+	return (paths);
+}
+
 char	**get_paths(char *const envp[])
 {
 	char	*path;
@@ -41,18 +59,20 @@ char	**get_paths(char *const envp[])
 	char	*tmp;
 	size_t	i;
 
-	i = 0;
 	path = get_path_envp(envp);
-	if (!path)
-	{
-		perror("Error");
-		exit(EXIT_FAILURE);
-	}
-	paths = ft_split(path, ':');
+	paths = split_path(path);
+	i = 0;
 	while (paths[i])
 	{
 		tmp = paths[i];
 		paths[i] = ft_strjoin(tmp, "/");
+		if (!paths[i])
+		{
+			free_err_join(path, tmp, &paths[i + 1]);
+			free(paths);
+			print_err(errno, __FILE__, __LINE__ - 3);
+			return (NULL);
+		}
 		free(tmp);
 		i++;
 	}
