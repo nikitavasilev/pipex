3de2ea14b8d96dd1c4dfb2cbfdd9fe7201412b0b
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-16 06:27:24 +0200

    heredoc prototypes + macros

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:53:52 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/14 02:19:07 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/16 06:04:59 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # define WRITE_END 1
 # define TEMP_READ_END 2
 # define USAGE "Usage: ./pipex [infile] [command] [command] ... [outfile]\n"
+# define US_HD "Usage: ./pipex here_doc [delimiter] [command] ... [outfile]\n"
 
 typedef struct s_args
 {
@@ -28,10 +29,11 @@ typedef struct s_args
 	int				outfile;
 	unsigned int	cmd_count;
 	pid_t			*cpid;
+	int				heredoc;
 }	t_args;
 
 char	**get_paths(char *const envp[]);
-char	***get_cmds(int argc, char *const argv[]);
+char	***get_cmds(t_args *args, int argc, char *const argv[]);
 void	free_args(t_args args);
 void	free_tab(char **tab);
 void	free_err_join(char *path, char *tmp, char **paths);
@@ -39,12 +41,16 @@ void	close_stdio(void);
 void	print_err(int errnum, char *str, int line);
 void	print_err_open(int errnum, char *path);
 void	exit_failure(t_args args, int fd[3], int status);
+void	init_tab(int *tab, unsigned int size);
 int		init_args(t_args *args, int ac, char *const av[], char *const ep[]);
+int		init_args_hd(t_args *args, int ac, char *const av[], char *const ep[]);
 int		is_dir(char *path);
 void	exec_cmd(t_args args, char **cmd, char *const envp[]);
 void	first_child(t_args args, char **cmd, char *const envp[], int fd[3]);
 void	inter_children(t_args args, char **cmd, char *const envp[], int fd[3]);
 void	last_child(t_args args, char **cmd, char *const envp[], int fd[3]);
-int		pipex(t_args args, char *const envp[]);
+int		pipex(t_args args, char *const envp[], char *const argv[]);
+void	here_doc(char *limiter, int fd[3], t_args args);
+int		exit_status(int status);
 
 #endif
