e7784131d51b52cdd5b2884b1ac83cf849184713
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-16 06:28:38 +0200

    here_doc handle

--- srcs/get_cmds.c
+++ srcs/get_cmds.c
@@ -6,30 +6,31 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 06:17:06 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/01 10:09:48 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/16 04:59:24 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "pipex.h"
 #include "libft.h"
 
-char	***get_cmds(int argc, char *const argv[])
+char	***get_cmds(t_args *args, int argc, char *const argv[])
 {
 	int		i;
 	char	***cmds;
 
-	cmds = (char ***)malloc(sizeof(char **) * (argc - 2));
+	cmds = (char ***)malloc(sizeof(char **) * (argc - 2 - args->heredoc));
 	if (!cmds)
 		return (NULL);
-	cmds[argc - 3] = NULL;
-	i = 0;
+	cmds[argc - (3 + args->heredoc)] = NULL;
+	i = args->heredoc;
 	while (i < argc - 3)
 	{
-		cmds[i] = ft_split(argv[i + 2], ' ');
-		if (!cmds[i])
+		cmds[i - args->heredoc] = ft_split(argv[i + 2], ' ');
+		if (!cmds[i - args->heredoc])
 		{
 			while (i--)
-				free(cmds[i]);
+				free(cmds[i + args->heredoc]);
 			free(cmds);
 			return (NULL);
 		}
