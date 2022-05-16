b8329cf02bf36a32520b7c4b895795c1d6b2a63f
Nikita VASILEV <vassilev.nikita@gmail.com>
2022-05-16 06:34:03 +0200

    here_doc handling

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,13 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 23:54:03 by nvasilev          #+#    #+#             */
-/*   Updated: 2022/05/12 06:02:19 by nvasilev         ###   ########.fr       */
+/*   Updated: 2022/05/16 04:58:47 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "libft.h"
 #include "pipex.h"
 
@@ -23,15 +24,21 @@ int	main(int argc, char *const argv[], char *const envp[])
 	status = 0;
 	if (argc >= 5)
 	{
-		if (!init_args(&args, argc, argv, envp))
+		if (ft_strncmp(argv[1], "here_doc", ft_strlen("here_doc")) == 0)
+		{
+			if (argc < 6)
+				return (ft_putstr_fd(US_HD, STDERR_FILENO), EXIT_FAILURE);
+			init_args_hd(&args, argc, argv, envp);
+		}
+		else if (!init_args(&args, argc, argv, envp))
 			exit_failure(args, NULL, EXIT_FAILURE);
-		status = pipex(args, envp);
+		status = pipex(args, envp, argv);
 		free_args(args);
 	}
 	else
 	{
 		ft_putstr_fd(USAGE, STDERR_FILENO);
-		status = 1;
+		status = EXIT_FAILURE;
 	}
 	close_stdio();
 	return (status);
